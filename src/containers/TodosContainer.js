abdbfa0a10dcf83f5f73529c182d537ff5365815
jiyoon-lee <jiyoon3421@gmail.com>
2022-06-19 23:59:57 +0900

    20220619 redux-actions react-redux redux

---
+++ src/containers/TodosContainer.js
@@ -0,0 +1,39 @@
+import React from 'react';
+// import { useCallback } from 'react';
+import { useSelector } from 'react-redux';
+import { changeInput, insert, toggle, remove } from '../modules/Todos';
+import Todos from '../components/Todo';
+import useActions from '../lib/useActions';
+
+const TodosContainer = () => {
+  const { input, todos } = useSelector(({ todos }) => ({
+    input: todos.input,
+    todos: todos.todos,
+  }));
+  // const dispatch = useDispatch();
+  // const onChangeInput = useCallback(
+  //   (input) => dispatch(changeInput(input)),
+  //   [dispatch],
+  // );
+  // const onInsert = useCallback((text) => dispatch(insert(text)), [dispatch]);
+  // const onToggle = useCallback((id) => dispatch(toggle(id)), [dispatch]);
+  // const onRemove = useCallback((id) => dispatch(remove(id)), [dispatch]);
+
+  const [onChangeInput, onInsert, onToggle, onRemove] = useActions(
+    [changeInput, insert, toggle, remove],
+    [],
+  );
+
+  return (
+    <Todos
+      input={input}
+      todos={todos}
+      onChangeInput={onChangeInput}
+      onInsert={onInsert}
+      onToggle={onToggle}
+      onRemove={onRemove}
+    />
+  );
+};
+
+export default React.memo(TodosContainer);
---
+++ src/lib/useActions.js
@@ -0,0 +1,15 @@
+// https://react-redux.js.org/api/hooks#recipe-useactions
+
+import { bindActionCreators } from 'redux';
+import { useDispatch } from 'react-redux';
+import { useMemo } from 'react';
+
+export default function useActions(actions, deps) {
+  const dispatch = useDispatch();
+  return useMemo(() => {
+    if (Array.isArray(actions)) {
+      return actions.map((a) => bindActionCreators(a, dispatch));
+    }
+    return bindActionCreators(actions, dispatch);
+  }, [dispatch, actions]);
+}
